2f9d04706af17a56f2af5f72d388345b89e22287
2nq <nate@bernich.com>
2022-03-25 14:48:10 -0400

    styling, misc updates

--- script.js
+++ script.js
@@ -1,30 +1,44 @@
-window.onload = async function() {
+window.onload = async ()=> {
 
-    const dayCounter = document.getElementById("days");
-    const hourCounter = document.getElementById("hours");
-    const minuteCounter = document.getElementById("minutes");
-    const secondCounter = document.getElementById("seconds");
+    const dayCounter = document.querySelector("#days");
+    const hourCounter = document.querySelector("#hours");
+    const minuteCounter = document.querySelector("#minutes");
+    const secondCounter = document.querySelector("#seconds");
 
+    // Set target time
     let target = new Date();
     target.setFullYear(2022, 3, 8);
     target.setHours(14);
-    target.setMinutes(51);
-    target.setSeconds(30);
+    target.setMinutes(46);
+    target.setSeconds(0);
     target.setMilliseconds(0);
 
     while (true) {
-
+        
         let delta = target.getTime() - Date.now();
 
-        if (delta < 1000) return singleDayTimer.innerText = "00:00:00";
+        // Display finished screen when time is here
+        if (delta < 1000) {
+            const heading = document.querySelector("#heading");
+            heading.innerHTML = 'The <span class="color">603 Challenge</span> is here!';
+            heading.setAttribute("role", "finished");
+            document.querySelector("#subheading").setAttribute("role", "finished");
+            document.querySelector("#timer").style.display = "none";
+            return;
+        }
 
-        dayCounter.innerText = String(Math.floor(delta / (3600000 * 24))).padStart(2, '0');
-        hourCounter.innerText = String(Math.floor(delta % (3600000 * 24) / 3600000)).padStart(2, '0');
-        minuteCounter.innerText = String(Math.floor(delta % 3600000 / 60000)).padStart(2, '0');
-        secondCounter.innerText = String(Math.floor(delta % 60000 / 1000)).padStart(2, '0');
+        // Update timer
+        dayCounter.innerText = pad(Math.floor(delta / (3600000 * 24)));
+        hourCounter.innerText = pad(Math.floor(delta % (3600000 * 24) / 3600000));
+        minuteCounter.innerText = pad(Math.floor(delta % 3600000 / 60000));
+        secondCounter.innerText = pad(Math.floor(delta % 60000 / 1000));
 
         await sleep(100);
     }
+}
+
+function pad(number) {
+    return String(number).padStart(2, '0')
 }
 
 function sleep(ms) {
